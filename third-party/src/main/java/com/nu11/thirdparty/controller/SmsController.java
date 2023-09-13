a64da8263c7e7fc30392f8075973eb47b89512f6
nu11zzZ <138970509+nu11zzZ@users.noreply.github.com>
2023-09-13 19:33:41 +0800

    Update SmsController.java

--- third-party/src/main/java/com/nu11/thirdparty/controller/SmsController.java
+++ third-party/src/main/java/com/nu11/thirdparty/controller/SmsController.java
@@ -30,7 +30,7 @@ public class SmsController {
                 String host = "https://dfsns.market.alicloudapi.com";
                 String path = "/data/send_sms";
                 String method = "POST";
-                String appcode = "569ae1411ee3419d87270a7b17924539";
+                String appcode = "";
                 Map<String, String> headers = new HashMap<String, String>();
                 //最后在header中的格式(中间是英文空格)为Authorization:APPCODE 83359fd73fe94948385f570e3c139105
                 headers.put("Authorization", "APPCODE " + appcode);
